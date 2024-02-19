c35df5a4cc6956bba4e1b9e432af22d003e6f158
Marnix Dessing <m.dessing@nijmegen.nl>
2024-02-19 13:27:04 +0100

    fix: loading of files fails on remote building machines

--- src/CorsaClient.ts
+++ src/CorsaClient.ts
@@ -1,15 +1,14 @@
 import { UUID } from 'crypto';
 import * as fs from 'fs';
-import * as path from 'path';
 
 export class CorsaClient {
   geefLijstZaakDocumenten(_corsaZaakUuid: UUID) {
-    const file = fs.readFileSync(path.resolve(__dirname, '../test/samples/geefLijstZaakdocumenten_Lv01.xml'));
+    const file = fs.readFileSync('test/samples/geefLijstZaakdocumenten_Lv01.xml');
     return file.toString('utf-8');
   }
 
   geefZaakDocument(_corsaDocumentUuid: UUID) {
-    const file = fs.readFileSync(path.resolve(__dirname, '../test/samples/geefZaakDocumentLezen_Lv0.xml'));
+    const file = fs.readFileSync('test/samples/geefZaakDocumentLezen_Lv0.xml');
     return file.toString('utf-8');
   }
 }
