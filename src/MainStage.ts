114ca5550c16c5e2e26e0fd00ac09a7ca3423207
Marnix Dessing <m.dessing@nijmegen.nl>
2024-02-19 14:20:28 +0100

    fix: permission boundary aspect

--- src/MainStage.ts
+++ src/MainStage.ts
@@ -1,7 +1,8 @@
-import { StackProps, Stage } from 'aws-cdk-lib';
+import { Aspects, Stack, StackProps, Stage } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { ApiStack } from './ApiStack';
 import { Configurable } from './Configuration';
+import { PermissionsBoundaryAspect } from '@gemeentenijmegen/aws-constructs';
 
 export interface MainStageProps extends StackProps, Configurable {}
 
@@ -9,10 +10,20 @@ export class MainStage extends Stage {
   constructor(scope: Construct, id: string, props: MainStageProps) {
     super(scope, id, props);
 
+    Aspects.of(this).add(new PermissionsBoundaryAspect());
+
+
     new ApiStack(this, 'api', {
       configuration: props.configuration,
     });
 
 
+  }
+}
+
+class ParameterStack extends Stack {
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+    
   }
 }
