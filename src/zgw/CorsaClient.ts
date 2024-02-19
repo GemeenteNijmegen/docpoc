7006c4a83ef6ab7404912ba4b46aa8009b1c9eb0
Joost van der Borg <j.van.der.borg@nijmegen.nl>
2024-02-19 14:43:07 +0100

    feat: Add corsa client test

--- src/zgw/CorsaClient.ts
+++ src/zgw/CorsaClient.ts
@@ -15,22 +15,27 @@ export class CorsaClient {
     });
   }
 
-  parseZaakDocument(xml: string): ZaakDocument {
-    const json = this.parser.parse(xml);
-    const doc = ZaakDocumentSchema.parse(json['soap:Envelope']['soap:Body']['zkn:edcLa01']['zkn:antwoord']['zkn:object']);
-    return doc;
-  }
-
   geefLijstZaakDocumenten(_corsaZaakUuid: UUID): ZaakDocumenten {
     const file = fs.readFileSync(path.resolve(__dirname, '../../test/samples/geefLijstZaakdocumenten_Lv01.xml'));
-    const json = this.parser.parse(file.toString('utf-8'));
-    console.debug('te3', json['soap:Envelope']['soap:Body']['zkn:zakLa01']['zkn:antwoord']['zkn:object']['zkn:heeftRelevant']);
-    const docs = ZaakDocumentenSchema.parse(json['soap:Envelope']['soap:Body']['zkn:zakLa01']['zkn:antwoord']['zkn:object']['zkn:heeftRelevant']);
+    const docs = this.parseZaakDocumenten(file);
     return docs;
   }
+
 
   geefZaakDocument(_corsaDocumentUuid: UUID) {
     const file = fs.readFileSync(path.resolve(__dirname, '../../test/samples/geefZaakDocumentLezen_Lv0.xml'));
     return this.parseZaakDocument(file.toString('utf-8'));
+  }
+
+  private parseZaakDocumenten(file: Buffer) {
+    const json = this.parser.parse(file.toString('utf-8'));
+    const docs = ZaakDocumentenSchema.parse(json['soap:Envelope']['soap:Body']['zkn:zakLa01']['zkn:antwoord']['zkn:object']['zkn:heeftRelevant']);
+    return docs;
+  }
+
+  private parseZaakDocument(xml: string): ZaakDocument {
+    const json = this.parser.parse(xml);
+    const doc = ZaakDocumentSchema.parse(json['soap:Envelope']['soap:Body']['zkn:edcLa01']['zkn:antwoord']['zkn:object']);
+    return doc;
   }
 }
