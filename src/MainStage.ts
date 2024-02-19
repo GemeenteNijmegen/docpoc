755cfc27f346e0c5e25dbbfa57055e73ecbb8f15
Marnix Dessing <m.dessing@nijmegen.nl>
2024-02-19 14:38:09 +0100

    chore: finish merging

--- src/MainStage.ts
+++ src/MainStage.ts
@@ -1,19 +1,9 @@
 import { PermissionsBoundaryAspect } from '@gemeentenijmegen/aws-constructs';
-<<<<<<< HEAD
-import { Aspects, Stack, StackProps, Stage } from 'aws-cdk-lib';
-import { Secret } from 'aws-cdk-lib/aws-secretsmanager';
-import { StringParameter } from 'aws-cdk-lib/aws-ssm';
-import { Construct } from 'constructs';
-import { ApiStack } from './ApiStack';
-import { Configurable } from './Configuration';
-import { Statics } from './Statics';
-=======
 import { Aspects, StackProps, Stage } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { ApiStack } from './ApiStack';
 import { Configurable } from './Configuration';
 import { ParameterStack } from './ParameterStack';
->>>>>>> da27f67f7bf5a4ecbe6ff28bdb516249babd8477
 
 export interface MainStageProps extends StackProps, Configurable {}
 
@@ -33,27 +23,3 @@ export class MainStage extends Stage {
 
   }
 }
-<<<<<<< HEAD
-
-class ParameterStack extends Stack {
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
-
-    new StringParameter(this, 'ssm_uitkering_2', {
-      stringValue: '-',
-      parameterName: Statics.ssmMTLSClientCert,
-    });
-
-    new StringParameter(this, 'ssm_uitkering_3', {
-      stringValue: '-',
-      parameterName: Statics.ssmMTLSRootCA,
-    });
-
-    new Secret(this, 'secret_2', {
-      secretName: Statics.secretMTLSPrivateKey,
-      description: 'mTLS certificate private key',
-    });
-  }
-}
-=======
->>>>>>> da27f67f7bf5a4ecbe6ff28bdb516249babd8477
