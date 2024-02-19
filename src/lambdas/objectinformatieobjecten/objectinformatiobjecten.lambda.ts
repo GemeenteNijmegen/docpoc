642c84833fdf04ad479093c786354d85918a157e
Marnix Dessing <m.dessing@nijmegen.nl>
2024-02-19 15:27:32 +0100

    chore: save two lines of code from deletion

--- src/lambdas/objectinformatieobjecten/objectinformatiobjecten.lambda.ts
+++ src/lambdas/objectinformatieobjecten/objectinformatiobjecten.lambda.ts
@@ -2,6 +2,10 @@ import { UUID } from 'crypto';
 import { APIGatewayProxyEvent } from 'aws-lambda';
 import { ObjectInformatieObjectenHandler } from './ObjectInformatieObjectenHandler';
 
+// TODO use this for mocking coras client
+// import * as geefLijstZaakdocumenten from '../../../test/samples/geefLijstZaakdocumenten_Lv01.xml';
+// import * as geefZaakDocument from '../../../test/samples/geefZaakdocumentLezen_Lv0.xml';
+
 function parseParameters(event: APIGatewayProxyEvent) {
   if (!event.pathParameters?.uuid) {
     throw Error('Required path parameter uuid not present');
